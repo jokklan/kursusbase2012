56cf658262e74a8df7516381d7647b6a3b0740a3
Johan Frølich <johanfrolich@gmail.com>
2012-02-01 14:53:40 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kursusbase2012::Application.config.secret_token = '54dc6aaaeb1fe90ceac329c0d85a96eca04025bb5dcb285a3e28d2770276d436f046d2ef5429b1999e5778113d6d127834f916a1663b78b7b2fda47c09764213'
