f4dc089e4b9496e0cb20bed69d0fe98e42222518
Johan Frølich <johanfrolich@gmail.com>
2012-02-29 13:17:46 +0100

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120229104705) do
+ActiveRecord::Schema.define(:version => 20120229105554) do
 
   create_table "course_relations", :force => true do |t|
     t.integer  "course_id"
