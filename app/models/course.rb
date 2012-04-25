2f73fd93e8e58d33f2c88df69e566d87746a291e
Johan Frølich <johanfrolich@gmail.com>
2012-04-25 17:08:55 +0200

    moved course helper methods from model to helper

--- app/models/course.rb
+++ app/models/course.rb
@@ -97,33 +97,6 @@ class Course < ActiveRecord::Base
       self.course_number
     end
   end
-  
-  def schedule_table_cell(sch)
-    schs = schedules.select {|s| s.block[1,2] == sch}
-    if schs.map {|s| s.block[0]}.include?("F") && schs.map {|s| s.block[0]}.include?("E")
-      css_class = "allyear"
-    elsif schs.map {|s| s.block[0]}.include?("F")
-      css_class = "spring"
-    elsif schs.map {|s| s.block[0]}.include?("E")
-      css_class = "autumn"
-    end
-    if css_class.blank?
-      "<td>#{sch}</td>".html_safe
-    else
-      "<td class=\"#{css_class}\"><strong>#{sch}</strong></td>".html_safe
-    end
-      
-  end
-  
-  def season_text()
-    if schedules.map {|s| s.block[0]}.include?("F") && schedules.map {|s| s.block[0]}.include?("E")
-      I18n.translate('seasons.spring') + " " + I18n.translate('and') + " " + I18n.translate('seasons.autumn')
-    elsif schedules.map {|s| s.block[0]}.include?("F")
-      I18n.translate('seasons.spring')
-    elsif schedules.map {|s| s.block[0]}.include?("E")
-      I18n.translate('seasons.autumn')
-    end
-  end
 
 	def prereq_for
 		rec_courses = []
