d0664f97ab932131f54bf88abf35d9b1de230efd
Johan Frølich <johanfrolich@gmail.com>
2012-06-06 10:08:23 +0200

    login from frontpage

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,7 @@
 class StudentsController < ApplicationController
 	def show
 	  unless @student = current_student
+	    @student = Student.new
 	    render "/home/index", layout: "home"
     end
 	end
