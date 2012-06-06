cac75be147fc9dc947b408fbd0df7e4ff57bef10
Andy Graulund <electricnet@gmail.com>
2012-06-06 12:07:41 +0200

    New pimpin' home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,44 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+var d    = $(document)
+var lang = (location.pathname.match(/^\/?(da|en)(\/|$)/) || [, "da"])[1]
+
+var strings = {
+	en: {
+		search: "Search",
+		login: "Log in",
+		cancel: "Cancel"
+	},
+	da: {
+		search: "Søgning",
+		login: "Log ind",
+		cancel: "Annuller"
+	}
+}
+
+d.ready(function(){
+	if($("body").is("#home")){
+		var loggingin = false
+		$("a[href^='/login'], a.cancel").click(function(){
+			if(loggingin){
+				$("#login").slideUp()
+				$("#searchform").slideDown()
+				$("#logintro").show()
+				$("a#toplogin").text(strings[lang].login)
+				$("#logo h1").text(strings[lang].search)
+				$("#searchform input[type='text']")[0].focus()
+			} else {
+				$("#login").slideDown()
+				$("#searchform").slideUp()
+				$("#logintro").hide()
+				$("a#toplogin").text(strings[lang].cancel)
+				$("#logo h1").text(strings[lang].login)
+				$("#login input[type='text']")[0].focus()
+			}
+			loggingin = !loggingin
+			return false
+		})
+	}
+})
